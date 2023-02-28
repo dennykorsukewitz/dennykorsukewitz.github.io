59e7ac81aa043c039180050b5d2edea301d06a0c
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2023-02-28 11:19:53 +0100

    wip.

--- .github/workflows/pages/data.sh
+++ .github/workflows/pages/data.sh
@@ -5,5 +5,6 @@ TOPIC="pages"
 
 REPOSITORIES=($(gh search repos --owner "$OWNER" --topic "$TOPIC" --jq '.[].name' --json name))
 for REPOSITORY in "${REPOSITORIES[@]}"; do
+  echo "Add $REPOSITORY to _data/repositories.yml"
   echo "- $REPOSITORY" >> _data/repositories.yml
 done
