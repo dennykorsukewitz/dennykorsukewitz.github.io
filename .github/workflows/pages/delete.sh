562d4c0674e1f984d7fb6d5426e5056fe94b0295
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2023-08-30 13:59:58 +0200

    Tidied.

--- .github/workflows/pages/delete.sh
+++ .github/workflows/pages/delete.sh
@@ -2,12 +2,16 @@
 
 REPOSITORIES=($(gh search repos --owner "dennykorsukewitz" --topic "pages" --jq '.[].name' --json name))
 REPOSITORIES+=("DK4")
+REPOSITORIES+=("dennykorsukewitz")
+REPOSITORIES+=("_posts")
 
 for REPOSITORY in "${REPOSITORIES[@]}"; do
   echo "Delete: $REPOSITORY"
   rm -Rf "$REPOSITORY"
 done
 
-rm -f _posts/*
+echo "Delete: _data/repositories.yml"
 rm -f _data/repositories.yml
-rm -Rf dennykorsukewitz
+
+
+touch _posts/.placeholder
