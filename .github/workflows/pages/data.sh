a6e73193c581f2ca6eec8abae69895534e978451
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2024-01-05 08:47:43 +0100

    Updated Workflow.

--- .github/workflows/pages/data.sh
+++ .github/workflows/pages/data.sh
@@ -3,9 +3,14 @@
 OWNER="dennykorsukewitz"
 mapfile -t REPOSITORIES < <(gh search repos --owner "$OWNER" --topic "pages" --jq '.[].name' --json name | sort)
 
-cat /dev/null > _data/repositories.yml
+PAGES='pages'
+if [ -z "$GITHUB_WORKSPACE" ];then
+  PAGES='.'
+fi
+
+cat /dev/null > "$PAGES"/_data/repositories.yml
 
 for REPOSITORY in "${REPOSITORIES[@]}"; do
-  echo "Add $REPOSITORY to _data/repositories.yml"
-  echo "- $REPOSITORY" >> _data/repositories.yml
+  echo "Add $REPOSITORY to "$PAGES"/_data/repositories.yml"
+  echo "- $REPOSITORY" >> "$PAGES"/_data/repositories.yml
 done
